733dfff81d6a6fe48e23f1ac1b6270b5e4662977
Christheoreo <chris.finlow@gmail.com>
2026-06-07 16:31:43 +0100

    feat(api, auth): add configuration management and update dependencies
    - Added @nestjs/config to both API and Auth services for environment variable management.
    - Updated package.json and package-lock.json to include axios, bcrypt, and jsonwebtoken dependencies.
    - Created .env.example file for Auth service with database and key paths.
    - Updated Dockerfile for Auth service to use Node.js 24-alpine.
    - Refactored app.module.ts in both services to include ConfigModule with global scope.

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    // load from K8s in production
+    ignoreEnvFile: process.env.NODE_ENV === 'production'
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
